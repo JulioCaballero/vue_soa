bb8953b4dd30505a42cfa4ee5569564b3e17a40d
JulioCaballero <173180@ids.upchiapas.edu.mx>
2020-06-23 17:35:48 -0500

    Modified testfile.txt

---
+++ Menu.java
@@ -0,0 +1,11 @@
+import java.io.File;
+
+public class Menu {
+	public static void main(String[] args) {
+		
+		String filePath = new File("").getAbsolutePath();
+		
+		System.out.println(filePath);
+	}
+}
+
